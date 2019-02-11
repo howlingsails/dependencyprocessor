69c9bc0c70b81f1f1b5c4181ab8d1a1f794d4421
Cap10Bill Nortman <bnortman@yahoo.com>
2019-02-10 23:56:42 -0800

    Cleanup

--- src/main/java/com/howlingsails/DependencyProcessor.java
+++ src/main/java/com/howlingsails/DependencyProcessor.java
@@ -5,7 +5,6 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.TreeSet;
 
 public class DependencyProcessor {
